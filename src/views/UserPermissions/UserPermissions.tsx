05557941fc649dba83f488594a81828dd25acf63
heldersousa <3x9equals27@gmail.com>
2021-07-18 19:09:48 +0200

    new proc userrole change

--- src/views/UserPermissions/UserPermissions.tsx
+++ src/views/UserPermissions/UserPermissions.tsx
@@ -1,22 +1,24 @@
 /* eslint-disabl */
 import styles from './UserPermissions.module.scss';
-import React, { useEffect, useState } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { InventartApi } from '../../services/api/InventartApi';
-import { Button, CircularProgress, Typography } from '@material-ui/core';
+import { Button, CircularProgress, MenuItem, Select, Typography } from '@material-ui/core';
 import { Permission } from '../../services/Authentication/Permission';
 import { PermissionManager } from '../../services/Authentication/PermissionManager';
 import { Column } from '@material-table/core';
 import { GridCore } from '../../components/GridCore/GridCore';
 import { Role } from '../../services/Authentication/Role';
+import { useTranslation } from 'react-i18next';
+import { couldStartTrivia } from 'typescript';
+import { values } from 'lodash';
 
 const UserPermissions = (api: InventartApi, permissionManager: PermissionManager) => {
+  const { t } = useTranslation();
   const [state, setState] = useState<{ loading: boolean, gridData: Array<any> }>({
     loading: true,
     gridData: []
   });
   const [authorized] = useState<boolean>(permissionManager.Check(Permission.EDIT_PERMISSIONS));
-
-  console.warn('UserPermissions');
 
   useEffect(() => {
     (async () => {
@@ -39,6 +41,77 @@ const UserPermissions = (api: InventartApi, permissionManager: PermissionManager
     })();
   }, [api, state.loading, authorized]);
 
+  const gridColumns: Array<Column<any>> = [
+    {
+      field: 'email',
+      title: 'E-mail',
+      type: 'string',
+    },
+    {
+      field: 'first_name',
+      title: 'First Name',
+      type: 'string',
+    },
+    {
+      field: 'last_name',
+      title: 'Last Name',
+      type: 'string',
+    },
+    {
+      field: 'role',
+      title: 'Role',
+      type: 'string',
+      render: (rowData: any) => {
+        if (rowData.is_guest_user === true) {
+          if (rowData.role === Role.GUEST) {
+            return (<div>Guest Enabled</div>);
+          }
+          if (rowData.role === Role.DISABLED) {
+            return (<div>Guest Disabled</div>);
+          }
+        }
+        return (
+          <div>{t(rowData.role)}</div>
+        );
+      }
+    },
+    {
+      title: 'Change',
+      type: 'string',
+      render: (rowData: any) => {
+        if (rowData.role === Role.CURATOR) {
+          return '';
+        }
+        if (rowData.is_guest_user === true) {
+          if (rowData.role === Role.GUEST) {
+            return (<Button variant={'contained'} color={'secondary'}>Disable</Button>);
+          }
+          if (rowData.role === Role.DISABLED) {
+            return (<Button variant={'contained'} color={'primary'}>Enable</Button>);
+          }
+        }
+        return (
+          <div>{roleSelector(rowData.guid, rowData.role)}</div>
+        );
+      }
+    }
+  ];
+
+  function roleSelector(guid: string, role: string): ReactNode {
+    return (
+      <Select value={role} onChange={(event: any) => onChangeRoleSelector(guid,event.target.value)}>
+        <MenuItem value={Role.DISABLED}>{t(Role.DISABLED)}</MenuItem>
+        <MenuItem value={Role.VISITOR}>{t(Role.VISITOR)}</MenuItem>
+        <MenuItem value={Role.CONTRIBUTOR}>{t(Role.CONTRIBUTOR)}</MenuItem>
+      </Select>);
+  }
+
+  function onChangeRoleSelector(userGuid: string, role: string): void {
+    console.warn(userGuid, role);
+    //WIP: call user rle change here
+    // curl -X POST "https://localhost:5001/user/FBAUL/roleChange/51654768-0967-48d3-b27a-ee4e0c2fcef3/role%3Adisabled"
+  }
+
   if (!authorized) {
     return <div className={styles.mainBody} >Access Denied</div>;
   }
@@ -57,65 +130,7 @@ const UserPermissions = (api: InventartApi, permissionManager: PermissionManager
 
 export default UserPermissions;
 
-const gridColumns: Array<Column<any>> = [
-  {
-    field: 'email',
-    title: 'E-mail',
-    type: 'string',
-  },
-  {
-    field: 'first_name',
-    title: 'First Name',
-    type: 'string',
-  },
-  {
-    field: 'last_name',
-    title: 'Last Name',
-    type: 'string',
-  },
-  {
-    field: 'role',
-    title: 'Role',
-    type: 'string',
-    render: (rowData: any) => {
-      if (rowData.role === Role.CURATOR) {
-        return 'Curator';
-      }
-      if (rowData.is_guest_user === true) {
-        if (rowData.role === Role.GUEST) {
-          return (<div>Guest Enabled</div>);
-        }
-        if (rowData.role === Role.DISABLED) {
-          return (<div>Guest Disabled</div>);
-        }
-      }
-      return (
-        <div>{rowData.role}</div>
-      );
-    }
-  },
-  {
-    title: 'Change',
-    type: 'string',
-    render: (rowData: any) => {
-      console.warn(rowData);
-      if (rowData.role === Role.CURATOR) {
-        return '';
-      }
-      if (rowData.is_guest_user === true) {
-        if (rowData.role === Role.GUEST) {
-          return (<Button variant={'contained'} color={'secondary'}>Disable</Button>);
-        }
-        if (rowData.role === Role.DISABLED) {
-          return (<Button variant={'contained'} color={'primary'}>Enable</Button>);
-        }
-      }
-      return (
-        <div>{rowData.role}</div>
-      );
-    }
-  }
-];
+
 
 /*
 email: "guest@inventart"
